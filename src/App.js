00dce0dfb47d36e37638c0703609b652e51251a5
Abhinas Dash <abhinasdash143@gmail.com>
2023-03-28 17:55:28 +0530

    Meals-Summery

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
 import Header from "./Components/Layout/Header";
-
+import Meals from "./Components/Meals/Meals";
 
 function App() {
-  
   return (
     <Fragment>
-      <Header/>
+      <Header />
+      <Meals />
     </Fragment>
   );
 }
