adcbe340fa2490040b669d6ccbd57f95d91c9d88
Abhinas Dash <abhinasdash143@gmail.com>
2023-03-28 15:51:49 +0530

    Header

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Header from "./Components/Layout/Header";
+
+
+function App() {
+  
+  return (
+    <Fragment>
+      <Header/>
+    </Fragment>
+  );
+}
+
+export default App;
